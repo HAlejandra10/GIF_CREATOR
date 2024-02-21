2d8074cf21af088b3382f081d174bf874c209551
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-20 19:25:18 -0500

    save each image to convert to a gif and to save all them adding rest of the images from the image second untill to create a gif and will save it

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ image_path_list = ['dog-1.png','dog-2.png']
 # Create a list of image objects
 image_list = [Image.open(file) for file in image_path_list]
 
+# Save the first image as a GIF file
+image_list[0].save(
+            'animation2.gif',
+            save_all=True,
+            append_images=image_list[1:], # append rest of the images
+            duration=1000, # in milliseconds
+            loop=0)
 
 
 
