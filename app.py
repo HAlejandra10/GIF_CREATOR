c1156380b4788bc7814d5abbf4925562bdce2b66
HeidyAPrimo <heidyprimo10@outlook.com>
2024-02-20 19:17:52 -0500

    use the module Image from PILLOW and it will open the file to iterate each file iinto the list path list

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from PIL import Image
 # Take list of paths for images
 image_path_list = ['dog-1.png','dog-2.png']
 
+# Create a list of image objects
+image_list = [Image.open(file) for file in image_path_list]
 
 
 
